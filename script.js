1ff22fc69db8e2ab59da8860e4f92ac2a3d52edb
ymendez1 <yesicamendez9510@gmail.com>
2021-07-22 21:19:03 -0500

    Primer repositorio

---
+++ script.js
@@ -0,0 +1,109 @@
+$(".Accenture").click(function(){
+    $(".Accenture").css("background", "blue")
+})
+$(".Accenture").dblclick(function(){
+    $(".Accenture").css("background", "white")
+})
+$(".Accenture").mouseover(function(){
+    $(".Accenture").css("font-size", "30px")
+})
+$(".Accenture").mouseout(function(){
+    $(".Accenture").css("font-size", "17px")
+})
+$(".ATKearney").mouseover(function(){
+    $(".ATKearney").css("color", "red")
+})
+$(".ATKearney").mouseout(function(){
+    $(".ATKearney").css("color", "blueviolet")
+})
+$(".ATKearney").click(function(){
+    $(".ATKearney").css("font-family", "Times New Roman")
+})
+$(".BainCompany").mouseover(function(){
+    $(".BainCompany").css("font-size", "10px")
+})
+$(".BainCompany").click(function(){
+    $(".BainCompany").css("background", "yellow")
+})
+$(".TheBostonConsultingGroup").click(function(){
+    $(".TheBostonConsultingGroup").css("color", "brown")
+})
+$(".TheBostonConsultingGroup").mouseover(function(){
+    $(".TheBostonConsultingGroup").css("width", "30px")
+})
+$(".TheBostonConsultingGroup").mouseout(function(){
+    $(".TheBostonConsultingGroup").css("width", "17px")
+})
+$(".DeloitteConsulting").mouseover(function(){
+    $(".DeloitteConsulting").css("background","Orange")
+})
+$(".DeloitteConsulting").mouseout(function(){
+    $(".DeloitteConsulting").css("background", "floralwhite")
+})
+$(".DeloitteConsulting").dblclick(function(){
+    $(".DeloitteConsulting").css("font-size", "10px")
+})
+$(".IBMGlobalBusinessServices").mouseover(function(){
+    $(".IBMGlobalBusinessServices").css("font-weight", "bold")
+})
+$(".IBMGlobalBusinessServices").mouseout(function(){
+    $(".IBMGlobalBusinessServices").css("font-family", "Lucida Sans")
+})
+$(".IBMGlobalBusinessServices").click(function(){
+    $(".IBMGlobalBusinessServices").css("width", "30px")
+})
+
+$(".KP").mouseover(function(){
+    $(".KP").css("text-decoration", "underline")
+})
+$(".KP").click(function(){
+    $(".KP").css("color", "black")
+})
+
+$(".McKinseyCompany").click(function(){
+    $(".McKinseyCompany").css("background", "pink")
+})
+$(".McKinseyCompany").dblclick(function(){
+    $(".McKinseyCompany").css("color", "white")
+})
+$(".Pricewaterhouse").click(function(){
+    $(".Pricewaterhouse").css("background", "pink")
+})
+$(".Pricewaterhouse").dblclick(function(){
+    $(".Pricewaterhouse").css("background", "white")
+})
+$(".Pricewaterhouse").mouseover(function(){
+    $(".Pricewaterhouse").css("font-size", "15px")
+})
+$(".Pricewaterhouse").mouseout(function(){
+    $(".Pricewaterhouse").css("font-size", "17px")
+})
+
+$(".ErnsYoungAdvisor").dblclick(function(){
+    $(".ErnsYoungAdvisor").css("font-family", "cursive")
+})
+
+$(".ErnsYoungAdvisor").mouseover(function(){
+    $(".ErnsYoungAdvisor").css("font-size", "25px")
+})
+
+
+
+$(".caja").mouseover(function(){
+    $(".caja").css("background","green")
+})
+$(".caja").mouseout(function(){
+    $(".caja").css("background", "floralwhite")
+})
+$(".caja2").mouseover(function(){
+    $(".caja2").css("background","blue")
+})
+$(".caja2").mouseout(function(){
+    $(".caja2").css("background", "floralwhite")
+})
+$(".caja3").mouseover(function(){
+    $(".caja3").css("background","yellow")
+})
+$(".caja3").mouseout(function(){
+    $(".caja3").css("background", "floralwhite")
+})
